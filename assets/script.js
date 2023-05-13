f40d893224a706ed9f3be8842264e5f7f59082de
Pablo Moreno <ehtomer@gmail.com>
2023-05-13 19:47:37 +0200

    up

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 /////////// IPHONE HEIGHT ///////////
+window.onload = mobileWindow();
 
 function mobileWindow() {
     let vh = window.innerHeight * 0.01;
@@ -6,7 +7,6 @@ function mobileWindow() {
     console.log("VH on mobiles", vh);
 }
 
-window.addEventListener("resize", mobileWindow, false);
 window.addEventListener("orientationchange", mobileWindow, false);
 
 
